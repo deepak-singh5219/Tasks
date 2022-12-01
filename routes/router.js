d094c6040e0f8e13032d0b31d2cbe618afc8c236
Deepak Singh <singhdeepak5219@gmail.com>
2022-12-01 20:38:40 +0530

    fixed searchtodo route

--- routes/router.js
+++ routes/router.js
@@ -1,11 +1,11 @@
 const express = require("express");
 const router = express.Router();
-const {createTodo, getTodos, updateTodo, deleteTodo, searchTodo} = require('../controllers/appcontrollers');
+const {createTodo, getTodos, updateTodo, deleteTodo, searchTodos} = require('../controllers/appcontrollers');
 
 router.get('/todo/getTodos', getTodos);
 router.post('/todo/createTodo', createTodo);
 router.put('/todo/updateTodo/:id', updateTodo);
 router.delete('/todo/deleteTodo/:id', deleteTodo);
-// router.get('/todo/searchTodo', searchTodo);
+router.get('/todo/searchTodo', searchTodos);
 
 module.exports = router;
