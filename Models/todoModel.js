c5dafee256c1267fbd92d1f0da059a77b149fc53
Deepak Singh <singhdeepak5219@gmail.com>
2022-12-02 19:35:25 +0530

    fixed overall database search index

--- Models/todoModel.js
+++ Models/todoModel.js
@@ -19,5 +19,6 @@ const todoSchema = new mongoose.Schema(
 }
 )
 
-todoSchema.index({'$**': 'text'});
+todoSchema.index({'$**': 'text'});   // index to search for alll strings in schema
+// todoSchema.index({name: 'text', 'tasks': 'text'});  // index to search only in tasks
 module.exports = mongoose.model("todo",todoSchema);
