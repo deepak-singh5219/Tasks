077f2098563f07f621634b979691661ba2f0b725
Deepak Singh <singhdeepak5219@gmail.com>
2022-12-08 21:12:32 +0530

    added routes back again

--- routes/router.js
+++ routes/router.js
@@ -5,6 +5,7 @@ const {createTodo, getTodos, updateTodo, deleteTodo, searchTodos} = require('../
 router.get('/todo/getTodos', getTodos);
 router.post('/todo/createTodo', createTodo);
 router.put('/todo/updateTodo/:id', updateTodo);
-
+router.delete('/todo/deleteTodo/:id', deleteTodo);
+router.get('/todo/searchTodo', searchTodos);
 
 module.exports = router;
